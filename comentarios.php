eee7f3bc4248494c09783ab6118308cca1b2b7de
Marco Novaes <marco-novaes@uol.com.br>
2024-04-23 19:22:57 -0300

    Create comentarios.php

---
+++ comentarios.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Curso PHP</title>
+</head>
+<body>
+
+	<!--Comentário Html -->
+<?php 
+
+	// Comentário de uma linha
+	echo 'Comando echo<br/>';
+
+	# Comentário de uma linha
+	print "Comando print<hr/>"
+
+    /*
+		Comentário de múltiplas linhas.
+		Este tipo de comentário permite que várias linhas sejam comentadas ao mesmo tempo.
+
+    */
+
+?>
+</body>
+</html>
