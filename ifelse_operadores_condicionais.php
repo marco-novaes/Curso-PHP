6781b69b8726875d96f3e989602f8e4d18d98c07
Marco Novaes <marco-novaes@uol.com.br>
2024-04-24 14:15:24 -0300

    Create ifelse_operadores_condicionais.php

---
+++ ifelse_operadores_condicionais.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Curso PHP</title>
+</head>
+<body>
+
+
+	<?php 
+
+			//==
+			/*if(2==2){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+
+			//===
+			/*if(2===2){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+	
+			//!= ou <>
+
+			/*if(5 != 6){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+			//!==
+			/*if(5 !== '5'){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+			//<
+
+			/*if(25 < 18){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+			//>
+			/*if(25 > 25){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+			//<=
+			/*if(25 <= 30){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/	
+
+			//>=
+			/*if(25 <= 30){
+				echo 'Verdadeiro';
+
+			}else{
+				echo 'Falso';
+			}*/
+
+			// Dica Pode omitir chaves mas sempre utilize o bloco de código com as chaves para manter organizado e legível
+			if('a' != 'a') {
+				echo 'Verdadeiro';
+				echo 'Segunda instrução';
+				echo 'Terceira instrução';
+	}
+	?>
+</body>
+</html>
